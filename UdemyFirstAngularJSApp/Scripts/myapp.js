6dcfe4908164eaf3d46d5934077ffb9f4ecaf627
dineshr <rdineshr@hotmail.com>
2017-02-05 20:55:48 -0700

    lecture 6

---
+++ UdemyFirstAngularJSApp/Scripts/myapp.js
@@ -0,0 +1,9 @@
+﻿var app = angular.module('MyApp', []);
+
+app.controller('MyController', function ($scope) {
+    $scope.MyVal = true;
+
+    $scope.MyFun = function () {
+        alert("Welcome to the Angular World!");
+    };
+});
